258eac80aa9aac6dfa88751ffb92a1043c006790
NoOverflow <julien.prost@epitech.eu>
2021-04-01 03:29:13 +0200

    Revert BMP back to true position on load

--- src/Assets/ImageLoader.cpp
+++ src/Assets/ImageLoader.cpp
@@ -5,6 +5,7 @@
 ** ImageLoader
 */
 
+#include <algorithm>
 #include "rts/memory/MemUtils.hpp"
 #include "rts/assets/ImageLoader.hpp"
 
@@ -21,6 +22,11 @@ BMP ImageLoader::loadBMPFromMem(const void *mem)
     ret.width = MemUtils::readMemory<unsigned int>(mem, 0x12);
     ret.height = MemUtils::readMemory<unsigned int>(mem, 0x16);
     ret.pixels = std::vector<Color>(pixels, pixels + (ret.width * ret.height));
+    std::reverse(ret.pixels.begin(), ret.pixels.end());
+    for (unsigned int y = 0; y < ret.height; y++) {
+        std::reverse(ret.pixels.begin() + (y * ret.width),
+            ret.pixels.begin() + ((y + 1) * ret.width));
+    }
     return ret;
 }
 
